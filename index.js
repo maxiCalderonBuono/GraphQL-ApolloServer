f71d54718446122e50ae4735285282d8632ceb3f
Maxi <calderonmaxi@outlook.com>
2022-09-10 11:15:24 +0200

    feat: Define data set

---
+++ index.js
@@ -0,0 +1,23 @@
+const persons = [
+  {
+    name: "Maxi",
+    phone: "045-91995343",
+    street: "Vesterbrogade",
+    city: "Copenhagen",
+    id: "36145334",
+  },
+  {
+    name: "Mariana",
+    phone: "045-71734562",
+    street: "Ostebro",
+    city: "Central Station",
+    id: "34745983",
+  },
+  {
+    name: "Nico",
+    phone: "045-91995082",
+    street: "Place",
+    city: "Heering",
+    id: "36164678",
+  },
+];
